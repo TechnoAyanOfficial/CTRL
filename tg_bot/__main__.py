80cf60cfd1576a684dda30b87bb2584e11e43592
Ayan Ansari <62174456+TechnoAyanOfficial@users.noreply.github.com>
2020-05-08 10:11:32 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -32,20 +32,16 @@ Hit /help for Avalaible Commands
 """
 
 HELP_STRINGS = """
-Hey there! My name is *{}*.
-I'm a modular group management bot with a few fun extras! Have a look at the following for an idea of some of \
-the things I can help you with.
+Here is the help for the @eLock_Bot
 
-*Main* commands available:
- - /start: start the bot
- - /help: PM's you this message.
- - /help <module name>: PM's you info about that module.
- - /settings:
-   - in PM: will send you your settings for all supported modules.
-   - in a group: will redirect you to pm, with all that chat's settings.
+ - /locktypes: a list of possible locktypes
 
-{}
-And the following:
+Admin only:
+ - /lock <type>: lock items of a certain type (not available in private)
+ - /unlock <type>: unlock items of a certain type (not available in private)
+ - /locks: the current list of locks in this chat.
+
+Ex. `/lock url` : Delete all messeges with URLs Sent By Non Admins
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
 
 IMPORTED = {}
