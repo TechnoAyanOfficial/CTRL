65b140413cf6babf4b726f204852c948ad99fb8f
Ayan Ansari <62174456+TechnoAyanOfficial@users.noreply.github.com>
2020-05-09 10:35:58 +0530

    update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -30,8 +30,6 @@ Read /help before using the bot
 """
 
 HELP_STRINGS = """
-Here is the help for the @eLock_Bot
-
  - /locktypes: a list of possible locktypes
 
 Admin only:
@@ -184,7 +182,7 @@ def help_button(bot: Bot, update: Update):
     try:
         if mod_match:
             module = mod_match.group(1)
-            text = "Here is the help for the *{}* module:\n".format(HELPABLE[module].__mod_name__) \
+            text = "Here is the help for the eLock Bot\n".format(HELPABLE[module8].__mod_name__) \
                    + HELPABLE[module].__help__
             query.message.reply_text(text=text,
                                      parse_mode=ParseMode.MARKDOWN,
